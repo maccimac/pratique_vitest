71d7e58d5dd7c4a12b2db3646b899a47bb5ff6d0
Macci Macaranas <macci.macaranas@impact.com>
2026-02-07 12:19:28 -0800

    Test DOM with happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
     test:{
         setupFiles: [
             'test/msw.setup.ts'
-        ]
+        ],
+        /**
+         * 5.22
+         * Two options: js-dom and happy-dom
+         * happy-dom is generally lighter and faster
+         */
+        environment: 'happy-dom'
     }
 })
